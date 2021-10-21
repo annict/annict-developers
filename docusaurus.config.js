8fd66b6fb64635918bde66768e85e95bfeb6edf9
Koji Shimba <me@shimba.co>
2021-10-22 05:12:37 +0900

    Fix path

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -24,7 +24,7 @@ const config = {
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
-          editUrl: 'https://github.com/kiraka/annict-developers/edit/main/docs/',
+          editUrl: 'https://github.com/kiraka/annict-developers/edit/main/',
         },
         blog: {
           showReadingTime: true,
